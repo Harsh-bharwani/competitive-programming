66135fd602704ead8ae86df616b96faed2962a1b
Harsh-bharwani <148901346+Harsh-bharwani@users.noreply.github.com>
2024-06-28 00:34:46 +0530

    Add files via upload

---
+++ goodArray.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+        int n,k;
+        cin>>n>>k;
+        cout<<2+(n-2)/k<<'\n';
+    }
+}
