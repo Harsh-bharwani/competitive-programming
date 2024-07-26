f82fd1e8a89a9ce5f548b1c90d574a7d2732d4c0
Harsh-bharwani <148901346+Harsh-bharwani@users.noreply.github.com>
2024-07-26 22:52:52 +0530

    Create legs.cpp

---
+++ legs.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int cow=n/4;
+        int chicken=(n%4)/2;
+        cout<<c+ch<<'\n';
+    }
+}
