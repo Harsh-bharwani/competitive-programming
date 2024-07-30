c30f07156d5f16a3d53d2bcae832d30d37700741
Harsh-bharwani <148901346+Harsh-bharwani@users.noreply.github.com>
2024-07-30 22:05:12 +0530

    Add files via upload

---
+++ makeItZero.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+         int n;
+         cin>>n;
+         vector<int> v(n);
+         for(int i=0; i < n; i++){
+               cin>>v[i];
+         }
+         if(n%2==0){
+            cout<<2<<'\n'<<1<<' '<<n<<'\n';
+         }
+         else{
+            cout<<4<<'\n'<<1<<' '<<n-1<<'\n'<<1<<' '<<n-1<<'\n'<<n-1<<' '<<n<<'\n'<<n-1<<' '<<n<<'\n';
+         }
+   }
+}
